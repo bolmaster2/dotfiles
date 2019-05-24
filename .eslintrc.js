7ee79ce7ec7b3f323c60f88b866ffb8f34ae214f
Joel Larsson <joellarsson@sent.at>
2019-05-24 12:27:04 +0200

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// This config only cares about parsing errors
+// Check https://eslint.org/docs/user-guide/configuring
+// ...and https://node.green/#ES2019 for nodejs version compatibility
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2019
+  }
+};
